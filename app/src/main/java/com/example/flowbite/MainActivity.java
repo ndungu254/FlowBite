f96d67aef6ad7a2504fee6f177099f38a8526c6f
collinkip <collinskiplangat@gmail.com>
2021-11-27 20:45:35 +0300

    Added main activity_main3.xml

--- app/src/main/java/com/example/flowbite/MainActivity.java
+++ app/src/main/java/com/example/flowbite/MainActivity.java
@@ -1,15 +1,61 @@
 package com.example.flowbite;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etMail,etPassword;
+    Button btLogin,btSignup;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String gitToken="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";
+        etMail=findViewById(R.id.eTMail);
+        etPassword=findViewById(R.id.eTpassword);
+        btLogin=findViewById(R.id.btLogin);
+        btSignup=findViewById(R.id.btSignup);
+
+
+        fAuth=FirebaseAuth.getInstance();
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mail=etMail.getText().toString().trim();
+                String password=etPassword.getText().toString().trim();
+
+                if (mail.isEmpty()) {
+                    etMail.setError("Email is empty");
+                }
+                if (password.isEmpty()) {
+                    etPassword.setError("Email is empty");
+                }
+                fAuth.createUserWithEmailAndPassword(mail,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isComplete()) {
+                            Toast.makeText(getApplicationContext(), "User Created", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),MainActivity3.class));
+                        }
+                        Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+        String gitToken="ghp_7rZw7muVj44qAWoDSe0bhm7Cu8DT2WN2kkklD8";
     }
 }
--- app/src/main/java/com/example/flowbite/MainActivity2.java
+++ app/src/main/java/com/example/flowbite/MainActivity2.java
@@ -2,13 +2,41 @@ package com.example.flowbite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity2 extends AppCompatActivity {
+    EditText etMail,etPassword,fName,lName,phone;
+    Button btnLogin,btnSignup;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        etMail=findViewById(R.id.etMail);
+        etPassword=findViewById(R.id.eTpassword);
+        btnLogin=findViewById(R.id.btnLogin);
+        btnSignup=findViewById(R.id.btnSignup);
+        lName=findViewById(R.id.lName);
+        fName=findViewById(R.id.fName);
+        phone=findViewById(R.id.phone);
+
+
+        fAuth=FirebaseAuth.getInstance();
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity2.this,MainActivity.class);
+                startActivity(intent);
+                etMail.setText("");
+                etPassword.setText("");
+            }
+        });
     }
 }
